1bd06d0bbb03dbf922d2acaad53eab56679ee7b8
Rafinha88 <rafaelsm88@gmail.com>
2016-04-29 09:20:48 -0300

    Implementing 10001st prime

---
+++ problem007.cpp
@@ -0,0 +1,39 @@
+/* 10001st prime
+ * By listing the firts prime numbers: 2, 3, 5, 7, 11, and 13,
+ * we can see that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ */
+
+#include <iostream>
+#include <vector>
+
+int main() {
+
+	const int limit = 10001;
+	std::vector<unsigned long int> primeNumbers;
+
+	unsigned long int number = 2;
+	primeNumbers.push_back(number);
+
+	number = 3;
+	while (primeNumbers.size() < limit) {
+		bool isPrime = true;
+		for (int i = 0; i < primeNumbers.size(); i++) {
+			if (number % primeNumbers[i] == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime) {
+			primeNumbers.push_back(number);
+		}
+		number = number + 2;
+	}
+
+	std::cout << primeNumbers.back() << std::endl;
+	
+
+	return 0;
+}
