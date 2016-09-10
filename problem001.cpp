a614339b4ca3b99ee74693dc515aa704b59d247d
Rafinha88 <rafaelsm88@gmail.com>
2016-09-10 23:50:49 +0100

    Refactoring function.

--- problem001.cpp
+++ problem001.cpp
@@ -7,20 +7,28 @@
  */
 
 #include <iostream>
+#include <set>
+
+int multiples(const std::set<int> &myset, const int &limit) {
+	int sum = 0;
+	for (int i = 0; i < limit; i++) {
+		for (std::set<int>::iterator it = myset.begin(); it != myset.end(); it++) {
+			if (i % *it == 0) {
+				sum += i;
+				break;
+			}
+ 		}
+	}
+	return sum;
+}
 
 int main() {
 	
-	int sum = 0;
-
-	for (int i = 0; i < 1000; i++) {
-		if (i % 3 == 0) {
-			sum += i;
-		} else if (i % 5 == 0) {
-			sum += i;
-		}
-	}
-
-	std::cout << sum << std::endl;
+	int myints[] = {3, 5};
+	std::set<int> myset (myints, myints + 2);
+	int limit = 1000;
+	
+	std::cout << multiples(myset, limit) << std::endl;
 
 	return 0;
 }
